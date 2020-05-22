ce2ce61465e667d5b898e08b0fb410fb811183af
AveryCameronUofR <35551198+AveryCameronUofR@users.noreply.github.com>
2020-05-22 10:46:11 -0400

    Updated Tests
    Removed test to check test coverage

--- test_githubActions.py
+++ test_githubActions.py
@@ -1,9 +1,17 @@
 import unittest
 import githubActions
-
-class MyTest(unittest.TestCase):
+class TestApp(unittest.TestCase):
     def test(self):
         self.assertEqual(githubActions.addOne(3), 4)
 
-    def testEven(self):
-        self.assertEqual(githubActions.makeEven(6), 6)
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTests(
+        unittest.TestLoader().loadTestsFromTestCase(TestApp)
+    )
+    return suite
+
+if __name__ == '__main__':
+   unittest.TextTestRunner(verbosity=2).run(suite())
+   
