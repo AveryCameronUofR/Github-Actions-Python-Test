0a21b8dc804bc9365e59585b21613b91fb02e6c3
AveryCameronUofR <35551198+AveryCameronUofR@users.noreply.github.com>
2020-05-27 15:06:14 -0400

    Changed testing framework to pytest
    Changed from unittest to pytest based on framework requirements for project

--- test_githubActions.py
+++ test_githubActions.py
@@ -1,21 +1,8 @@
-import unittest
+import pytest
 import githubActions
-class TestApp(unittest.TestCase):
-    def test(self):
-        self.assertEqual(githubActions.addOne(3), 4)
+class TestClass:
+    def test_one(self):
+        assert githubActions.addOne(3) == 4
 
     def testEven(self):
-        self.assertEqual(githubActions.makeEven(6), 6)
-
-    def testEven4(self):
-        self.assertEqual(githubActions.makeEven(3), 4)
-
-def suite():
-    suite = unittest.TestSuite()
-    suite.addTests(
-        unittest.TestLoader().loadTestsFromTestCase(TestApp)
-    )
-    return suite
-
-if __name__ == '__main__':
-   unittest.TextTestRunner(verbosity=2).run(suite())
+        assert githubActions.makeEven(6) == 6
